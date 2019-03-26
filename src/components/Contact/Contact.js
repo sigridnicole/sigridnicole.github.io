308dbc8e83ea6295d363400da9d8e97eeb3e9533
Sigrid Nicole Ramos <sigridncl@gmail.com>
2019-03-26 07:09:25 +0400

    added contact links

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Git from './icons/github.png';
+import LinkedIn from './icons/linkedin.png';
+import Mail from './icons/mail.png';
+import './Contact.css';
 
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <img src={Git} alt="Github" onClick={() => window.location.href = "https://github.com/sigridnicole"} ></img>
+      <img src={LinkedIn} alt="LinkedIn" onClick={() => window.location.href = "https://www.linkedin.com/in/sigridnicole/"} ></img>
+      <img src={Mail} alt="Mail" onClick={() => window.location.href = "mailto:sigridncl@gmail.comn"} ></img>
     </div>
-   );
+  );
 };
 
 export default Contact;
